c1432cde6ce8b90bf56e8de2d21c34690ecfbd22
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-11-10 22:08:26 +0530

    Create array.java
    array.java

---
+++ array.java
@@ -0,0 +1,20 @@
+import java.util.*; class array{
+public static void main(String arg[]){ int arr[]=new int[10] ;
+Scanner sc= new Scanner (System.in);
+
+//Ruchi declared array arr[0]=sc.nextInt(); arr[1]=sc.nextInt(); arr[2]=sc.nextInt(); arr[3]=sc.nextInt();
+
+System.out.println("arr[0]:"+arr[0]);
+System.out.println("arr[1]:"+arr[1]);
+System.out.println("arr[2]:"+arr[2]);
+System.out.println("arr[3]:"+arr[3]);
+
+//anjali printed array System.out.println("percentage="+(arr[0]+arr[1]+arr[2]+arr[3])/4);
+ 
+
+//Neha printed  size System.out.println(arr.length);
+
+//iterating array size ##ADII for(int i = 0;i<arr.length;i++){ system.out.println(arr[i];
+}
+}
+}
